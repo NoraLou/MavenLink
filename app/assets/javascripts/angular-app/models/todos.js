a8becdb7c20144ebac9f50532f412dbc01b5c49a
NoraLou <nora.humpage@gmail.com>
2015-11-07 12:55:15 -0800

    added angular config, todos model

---
+++ app/assets/javascripts/angular-app/models/todos.js
@@ -0,0 +1,13 @@
+angular
+  .module('app')
+  .factory('Todo', function($resource) {
+
+    var Todo = $resource('http://localhost:3000/api/v1/todos/:id.json', {id: '@id'}, {
+      update: {
+        method: 'PUT'
+      }
+    });
+
+    return Todo;
+  });
+
