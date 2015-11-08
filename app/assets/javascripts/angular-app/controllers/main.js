927d13fd77577b58df7126aa0a0b63a81ec5e7e0
NoraLou <nora.humpage@gmail.com>
2015-11-07 22:18:24 -0800

    added save function

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -1,8 +1,8 @@
 angular
   .module('app')
-  .controller('MainCtrl', ['Todo', '$scope','$routeParams', function(Todo, $scope, $routeParams,){
-
-    $scope.post = new Todo();
+  .controller('MainCtrl', ['Todo', '$scope','$routeParams', function(Todo, $scope, $routeParams){
+    //ng-model todo is by default new
+    $scope.todo = new Todo();
     $scope.todos = Todo.query();
 
     // $scope.addTodo = function(){
@@ -11,11 +11,16 @@ angular
     // }
 
     $scope.save = function(todo){
-
+      if(!todo.id){
+        $scope.todo.$save().then(function(response){
+          $scope.todos.push(response)
+        });
+      }
+      $scope.todo = new Todo();
     }
 
 
 
-    // $scope.addTodo =
+
 
   }]);
