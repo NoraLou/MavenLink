979a822049c3c74399eadd9784f3e729eab6b73a
NoraLou <nora.humpage@gmail.com>
2015-11-07 21:48:05 -0800

    fixed css alignment when resizing browser window, about to add methods to controller

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -1,7 +1,21 @@
 angular
   .module('app')
-  .controller('MainCtrl', ['Todo', '$scope', function(Todo, $scope){
+  .controller('MainCtrl', ['Todo', '$scope','$routeParams', function(Todo, $scope, $routeParams,){
 
+    $scope.post = new Todo();
     $scope.todos = Todo.query();
+
+    // $scope.addTodo = function(){
+    //   $scope.todo = new Todo();
+    //   $scope.editing = false;
+    // }
+
+    $scope.save = function(todo){
+
+    }
+
+
+
+    // $scope.addTodo =
 
   }]);
